12cf49a025383c434dc3cff821fcb8c574391cc5
stefanoboveri <61866824+stefanoboveri@users.noreply.github.com>
2025-11-12 13:29:21 +0300

    Delete

--- agent-monitoring-app-blueprint/agent-monitoring-app/src/config/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  clientId: '62451477-d58c-470f-853e-38f8013fcce5',
-  // redirectUri: 'https://stefanoboveri.github.io/agent-monitoring-app-blueprint/'
-  redirectUri: 'http://localhost:8080/'
-}
